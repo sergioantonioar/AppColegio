a5928ae0bff03649752f5fb708301fe9d395a121
Sergio <111243440+sergioantonioar@users.noreply.github.com>
2026-07-18 18:33:20 -0500

    add necesary dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -79,4 +79,8 @@ dependencies {
     implementation("io.coil-kt:coil-compose:2.6.0")
     implementation("com.squareup.okhttp3:logging-interceptor:4.12.0")
 
+    implementation(platform("com.google.firebase:firebase-bom:34.0.0"))
+    implementation("com.google.firebase:firebase-firestore")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-play-services:1.10.2")
+
 }
